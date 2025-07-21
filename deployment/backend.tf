d3a79137b8e8c6d50dbb62e031b6f1637ec417eb
Maeva ADAMS <adt_maeva.adams@vincic.onmicrosoft.com>
2025-07-21 17:43:20 +0200

    updates

--- deployment/backend.tf
+++ deployment/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-│   backend "local" {}
+  backend "azurerm" {
+    resource_group_name  = "edf-poc"
+    storage_account_name = "staedfpoc" # Must be globally unique
+    container_name       = "poc-edf"
+    key                  = "terraform.tfstate"
+  }
 }
